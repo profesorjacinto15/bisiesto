97f32d50cd2f0f436123efc7b9694214afe58252
profesorjacinto15 <profesor.jacinto15@gmail.com>
2022-04-08 01:03:46 +0100

    Commit comentario método main

--- src/bisiesto/bisiesto.java
+++ src/bisiesto/bisiesto.java
@@ -25,6 +25,11 @@ public class bisiesto {
 			
 	}
 	
+	/**
+	 * Metodo principal
+	 * @param args
+	 */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner tec = new Scanner(System.in);
